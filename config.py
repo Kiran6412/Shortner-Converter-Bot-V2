9aa4b822c7c3b49147b83b47d5b66b4fd4919b8c
Kiran6412 <136492278+Kiran6412@users.noreply.github.com>
2023-06-16 22:22:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,16 +7,17 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "22313424"))
+API_HASH = os.environ.get("API_HASH", "2cbd9a90ac99583580d9ad6150701045")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "
+6256949277:AAEsPCcINvVCpNPoX17LJZVeGYzu0qgpczI")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5268607724")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "ghhh")
 DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5268607724")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(Id Owned Id)
+ADMINS.append(5268607724)
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
